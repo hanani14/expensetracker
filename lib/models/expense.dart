99d6b2c7ab6d81081ac7a4b081dd3dc3233ef1cf
fhanani14 <fhanani841@gmail.com>
2023-09-03 00:50:40 +0800

    Initial commit

---
+++ lib/models/expense.dart
@@ -0,0 +1,12 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+class Expense{
+  final String title;
+  final double amount;
+  final String id;
+  final DateTime date;
+  Expense({required this.title,required this.amount,required this.date})
+  : id = uuid.v4();
+}
