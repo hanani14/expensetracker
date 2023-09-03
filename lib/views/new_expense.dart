f9337a0f2c617f6fe688b25e5d450a3bd0266dbb
fhanani14 <fhanani841@gmail.com>
2023-09-03 15:47:11 +0800

    adding a dropdown button

--- lib/views/new_expense.dart
+++ lib/views/new_expense.dart
@@ -1,7 +1,10 @@
 import 'package:expensetracker/global/separator.dart';
+import 'package:expensetracker/models/expense.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:intl/intl.dart';
+
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -13,12 +16,18 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
-  void _presentDatePicker(){
+  void _presentDatePicker()async{
     final now = DateTime.now();
     final firstDate = DateTime(now.year -1, now.month, now.day);
-    showDatePicker(context: context, initialDate: now, firstDate: firstDate, lastDate: now);
-    
+    final pickedDate = await showDatePicker(context: context, initialDate: now, firstDate: firstDate, lastDate: now);
+    //first method
+    // .then((value) { //once value is available });
+    setState(() {
+      _selectedDate = pickedDate;
+    });
   }
   // var _enteredTitle = '';
   
@@ -59,13 +68,26 @@ class _NewExpenseState extends State<NewExpense> {
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const Text('Selected Date'),
+              Text(_selectedDate ==null? 'No date selected': formatter.format(_selectedDate!)),
               IconButton(onPressed: _presentDatePicker, icon: const Icon(Icons.calendar_month))
             ],
           ))
         ],),
-       
+       Separator().heightSeperator(16),
         Row(children: [
+          DropdownButton(
+            value: _selectedCategory,
+            items: Category.values.map((category) => 
+              DropdownMenuItem(value: category,child: Text(category.name.toUpperCase()))).toList(), 
+            onChanged: (value){
+                if(value == null){
+                  return;
+                }
+                setState(() {
+                  _selectedCategory = value;
+                });
+          }),
+          const Spacer(),
           TextButton(onPressed: (){
             Navigator.pop(context);
           }, child: const Text('Cancel')),
