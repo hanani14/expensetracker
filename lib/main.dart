8ae23e824d118ca82392a53bac4974bc27f0018b
fhanani14 <fatin@cloone.com.my>
2023-09-04 23:17:19 +0800

    color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:expensetracker/views/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.blue);
 void main() {
   runApp(
      MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData().copyWith(useMaterial3: true,
-        scaffoldBackgroundColor: Colors.amberAccent),
+        colorScheme:kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer),
+          
+        ),
       home: Expenses(),
     ),
   );
