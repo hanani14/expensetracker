daad585b4cbbe36b2b09feb8718a0069df9df1af
fhanani14 <fhanani841@gmail.com>
2023-09-04 23:26:50 +0800

    theme text

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,20 @@ void main() {
       debugShowCheckedModeBanner: false,
       theme: ThemeData().copyWith(useMaterial3: true,
         colorScheme:kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal:16,vertical: 8)
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(fontWeight: FontWeight.normal,color: kColorScheme.onSecondaryContainer,fontSize: 14)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom(backgroundColor: kColorScheme.primaryContainer)),
+        appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer),
           
         ),
-      home: Expenses(),
+      home: const Expenses(),
     ),
   );
 }
