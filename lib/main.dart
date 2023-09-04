9db1c8ce2a32dac90ce7c0e5656688b89317d35a
fhanani14 <fhanani841@gmail.com>
2023-09-04 23:42:58 +0800

    adding dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,18 @@ import 'package:expensetracker/views/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.blue);
+
+var kDarkColorScheme = ColorScheme.fromSeed(brightness: Brightness.dark,seedColor: Color.fromARGB(255, 28, 13, 122));
 void main() {
   runApp(
      MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(  
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal:16,vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom(backgroundColor: kDarkColorScheme.primaryContainer,foregroundColor: kDarkColorScheme.onPrimaryContainer)),
+        useMaterial3: true,colorScheme: kDarkColorScheme),
       debugShowCheckedModeBanner: false,
       theme: ThemeData().copyWith(useMaterial3: true,
         colorScheme:kColorScheme,
